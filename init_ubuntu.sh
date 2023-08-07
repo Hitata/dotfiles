acd3d13c34db6fa2fa9ad481bab4c408428e834e
Trung H. Tran <tranhoangtrung.nob@gmail.com>
2023-08-07 18:26:31 +0700

    ss

---
+++ init_ubuntu.sh
@@ -0,0 +1,8 @@
+sudo apt install kitty
+sudo apt install fish
+sudo apt install tmux
+sudo apt install neovim
+sudo apt install fnm
+## curl -fsSL https://fnm.vercel.app/install | bash
+sudo apt install fzf
+
